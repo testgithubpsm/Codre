097a85b6bac642eff395cad53f091fe9d82a993a
testgithubpsm <120446280+testgithubpsm@users.noreply.github.com>
2022-12-24 10:45:23 +0530

    Add files via upload

---
+++ Program101.c
@@ -0,0 +1,47 @@
+/*
+input = row-4
+       clm =4
+
+1 2 3 4
+1 2 3 4
+1 2 3 4
+1 2 3 4
+
+*/
+
+//Templet
+
+#include<stdio.h>
+void Display(int iRow, int iCol)
+{
+   int i=0;
+   int j=0;
+   for(i=1; i<=iRow;i++)
+   {
+    for(j=1;j<=iCol;j++)
+    {
+        printf("%d\t",j);
+
+    }
+   }
+   
+
+}
+
+
+
+int main()
+{
+    int iValue1=0,iValue2=0;
+
+    printf("Enter the number of rows : \n");
+    scanf("%d",&iValue1); 
+
+    printf("Enter the number of columns : \n");
+    scanf("%d",&iValue2);
+
+    Display(iValue1,iValue2);
+
+
+    return 0;
+}
